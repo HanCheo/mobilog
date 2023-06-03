3fd0e8e47c6058d463122a6f6a304acda766e2b5
moby <moby@101.inc>
2023-06-03 18:16:27 +0900

    fix: dotenv init

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const dotenv = require('dotenv-webpack')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
 module.exports = withBundleAnalyzer({
   staticPageGenerationTimeout: 300,
+  webpack: (config) => {
+    if (process.env.NODE_ENV !== 'production') {
+      config.plugins.push(new dotenv({ silent: true }))
+    }
+    return config
+  },
   images: {
     domains: [
       'www.notion.so',
