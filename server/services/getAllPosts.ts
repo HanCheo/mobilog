945dc1bfafe3b4638b5783562324cbb255e78fda
moby <moby@101.inc>
2023-06-25 17:57:01 +0900

    fix: properties error

--- server/services/getAllPosts.ts
+++ server/services/getAllPosts.ts
@@ -5,7 +5,7 @@ import * as config from '@/config/config'
 const TYPE_PROPERTIES_NAME = 'xnm['
 
 const isPostType = (page: types.Block) => {
-  return page?.properties[TYPE_PROPERTIES_NAME]?.[0][0] === 'Post'
+  return page?.properties?.[TYPE_PROPERTIES_NAME]?.[0][0] === 'Post'
 }
 
 export const getPostsCanonical = async () => {
