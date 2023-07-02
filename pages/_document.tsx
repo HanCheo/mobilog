743551f02d6815251147a4d37ef26f5979b9409b
moby <moby@101.inc>
2023-07-02 17:14:01 +0900

    fix: meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,14 @@ export default () => {
           <link rel='shortcut icon' href='/favicon.ico' />
           <link rel='icon' type='image/png' sizes='32x32' href='favicon.png' />
           <link rel='manifest' href='/manifest.json' />
+          <meta
+            name='google-site-verification'
+            content='q4oalwUGELyfH7huZ4Ly1FBTWFbyoPZ6LbitF_76THg'
+          />
+          <meta
+            name='naver-site-verification'
+            content='f27a9e9c59cf024a065a45ccd1f034304ec66a43'
+          />
         </Head>
 
         <body>
