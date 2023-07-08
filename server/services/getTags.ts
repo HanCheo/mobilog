8bf3c53e7d9d7a166292b6227750edf42d7be58f
moby <moby@101.inc>
2023-07-08 10:27:41 +0900

    feat: tag cache

--- server/services/getTags.ts
+++ server/services/getTags.ts
@@ -1,4 +1,5 @@
 import { notionhqClient } from '@/server/infra'
+import { kv } from '@vercel/kv'
 
 type GetTagsResponse = {
   id: string
@@ -7,6 +8,12 @@ type GetTagsResponse = {
 }[]
 
 export const getTags = async (): Promise<GetTagsResponse> => {
+  const tagInfo = await kv.get<GetTagsResponse>('getTags')
+
+  if (tagInfo) {
+    return tagInfo
+  }
+
   const collectionInfo = await notionhqClient.databases.retrieve({
     database_id: process.env.NOTIONHQ_DATABASE_ID
   })
@@ -14,6 +21,10 @@ export const getTags = async (): Promise<GetTagsResponse> => {
   if (collectionInfo.properties.Tags.type !== 'multi_select') {
     throw new Error('Tags is not multi-select type')
   }
+
+  await kv.set('getTags', collectionInfo.properties.Tags.multi_select.options, {
+    ex: 60 * 60
+  })
 
   return collectionInfo.properties.Tags.multi_select.options
 }
