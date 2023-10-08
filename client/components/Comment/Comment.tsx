32bf3aa4fa5d74c72077d41910264389c3020053
Moby <05.hanch.it@gmail.com>
2023-10-08 20:58:01 +0900

    fix: post footer background color

--- client/components/Comment/Comment.tsx
+++ client/components/Comment/Comment.tsx
@@ -6,21 +6,23 @@ export const Comment: FC = () => {
   const { isDarkMode } = useTheme()
 
   return (
-    <div className='m-auto px-6 max-w-1200 w-full pb-5 z-10 bg-bg-color-default'>
-      <Giscus
-        repo='HanCheo/mobilog'
-        repoId='R_kgDOJo538Q'
-        category='Announcements'
-        categoryId='DIC_kwDOJo538c4CXAhg'
-        mapping='pathname'
-        strict='0'
-        reactions-enabled='1'
-        emit-metadata='0'
-        input-position='top'
-        theme={isDarkMode ? 'dark_dimmed' : 'light'}
-        lang='en'
-        loading='lazy'
-      />
+    <div className='w-full px-6 pb-5 z-10 bg-bg-color-default'>
+      <div className='m-auto max-w-1200 w-full'>
+        <Giscus
+          repo='HanCheo/mobilog'
+          repoId='R_kgDOJo538Q'
+          category='Announcements'
+          categoryId='DIC_kwDOJo538c4CXAhg'
+          mapping='pathname'
+          strict='0'
+          reactions-enabled='1'
+          emit-metadata='0'
+          input-position='top'
+          theme={isDarkMode ? 'dark_dimmed' : 'light'}
+          lang='en'
+          loading='lazy'
+        />
+      </div>
     </div>
   )
 }
